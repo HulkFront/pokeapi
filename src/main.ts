99662cec4f171be75d29247f6d8a409344dd4343
HulkFront <glauber.araujo@edu.pucrs.br>
2023-07-30 22:17:34 -0300

    fix(pokeapi-04): remove comentários

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
-// 1. Importe o componente RouterView
 import RouterView from './components/RouterView.vue';
 
-// 2. Crie um novo aplicativo Vue
 const app = createApp(App);
 
-// 3. Registre o componente RouterView globalmente
 app.component('RouterView', RouterView);
 
-// 4. Monte o aplicativo no elemento raiz do DOM
 app.mount('#app');
